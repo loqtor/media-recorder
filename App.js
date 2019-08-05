e8356859b7247aa4077d19fce4e2b0e63ecb5139
Martin Martinez <martin.martinez.sanchez@gmail.com>
2019-08-05 18:35:03 +1200

    Began to isolate recorder as its own thing from another app (itll-be-alright).

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Recorder } from './components/recorder';
+
+function App() {
+  if (!navigator.mediaDevices) {
+    return (<div className="App">Sorry, this app is not supported by your browser or device.</div>);
+  }
+
+  const onStart = () => {
+    console.log('It has started!');
+  }
+
+  const onDataAvailable = (e) => {
+    console.log('On data available: ', e.data)
+  }
+
+  const onStop = (audioUrl, recorderState) => {
+    console.log('I got the audio and the state: ', audioUrl, recorderState);
+  }
+
+  return (
+    <div className="App">
+      <h1>Recorder Test</h1>
+      <Recorder
+        onStart={onStart}
+        onDataAvailable={onDataAvailable}
+        onStop={onStop}
+      />
+    </div>
+  );
+}
+
+export default App;
